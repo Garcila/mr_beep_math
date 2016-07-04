1fa85e8e502231d20defbe8c63c6fd11b84fe99d
German Arcila <germanarcila@gmail.com>
2016-07-04 15:07:45 -0400

    basic layout set, starting js

--- mr_beep_math.js
+++ mr_beep_math.js
@@ -1,1 +1,8 @@
- 
+$(document).ready(function(){
+ $(document).on('click', '.calculate',function(e) {
+    $('.question').empty();
+    $(function () {
+      console.log('zapato')
+    })
+  })
+})
